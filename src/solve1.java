99a94d37481f277285b6427d840dba87ee868575
dell <aditya.chauhan_cs18@gla.ac.in>
2019-09-03 21:43:40 +0530

    lab4-solve1

---
+++ src/solve1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class solve1 {
+    public static void main(String[] args) {
+        double sum=0;
+        sum=2+3-5*2/5;
+        System.out.println(sum);
+    }
+}
