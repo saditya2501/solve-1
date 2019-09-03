f16285ab0eda937308ca303eec717d41f059399f
dell <aditya.chauhan_cs18@gla.ac.in>
2019-09-03 21:31:10 +0530

    lab4-02

---
+++ src/display2.java
@@ -0,0 +1,8 @@
+
+
+import java.util.Scanner;
+public class display2 {
+    public static void main(String[] args) {
+        System.out.println(" Good Evening,\n Welcome to Programming");
+    }
+}
